b075c2dc5668e61724e1071adc302a08e27d6ac6
Umar Farooq <140904339+Umar-Farooq-2112@users.noreply.github.com>
2025-02-19 09:23:28 +0500

    main

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -12,7 +12,7 @@ export const Banner = () => {
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Generative AI Engineer","Computer Vision Engineer", "Flutter Developer",];
+  const toRotate = [ "Computer Vision Engineer", "Flutter Developer"];
   const period = 2000;
 
   useEffect(() => {
